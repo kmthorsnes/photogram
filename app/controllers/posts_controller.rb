a1f5376bd78379d7db2378abfbdfa125a44a02a4
kalle <kmthorsnes@gmail.com>
2018-03-17 23:18:00 +0100

    pass test two

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,14 @@ class PostsController < ApplicationController
   end
 
   def create
-  @post = Post.create(post_params)
-  redirect_to @post
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to @post
+    else
+      flash[:alert] = "Halt, you fiend! You need an image to post here!"
+      render :new
+    end
   end
 
   def show
