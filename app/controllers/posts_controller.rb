385c2ba6b759aefa4b67a3f8eafa2ab32eeb6981
kalle <kmthorsnes@gmail.com>
2018-03-17 23:07:52 +0100

    creating posts green

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,11 @@ class PostsController < ApplicationController
 
   def create
   @post = Post.create(post_params)
+  redirect_to @post
+  end
+
+  def show
+  @post = Post.find(params[:id])
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
-class Post < ActiveRecord::Base
-  has_attached_file :image, styles: { :medium => "640x" }
+class Post < ApplicationRecord
+	validates :image, presence: true
 
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	has_attached_file :image, styles: { :medium => "640px" }
+
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
