4e264511056bf359743f06f53258d98f8ec0c9b0
kalle <kmthorsnes@gmail.com>
2018-04-27 12:39:20 +0200

    updated gems

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
-
+  extend Devise::Models
   has_attached_file :image, styles: { :medium => "640x" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
